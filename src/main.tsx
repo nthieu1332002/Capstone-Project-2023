345a0c72fb2c3592d57af02a0dc03f8cfb001441
Trung Hieu <nthieu1332002@gmail.com>
2023-05-07 15:27:57 +0700

    base code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
---
+++ src/store/store.ts
@@ -0,0 +1,32 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+// import contractSlice from "./contract/contractSlice";
+// import residentSlice from "./resident/residentSlice";
+// import serviceSlice from "./service/serviceSlice";
+// import userSlice from "./user/userSlice";
+// import flatSlice from "./flat/flatSlice";
+// import buildingSlice from "./building/buildingSlice";
+// import adminSlice from "./admin/adminSlice";
+// import dashboardSlice from "./dashboard/dashboardSlice";
+// import billSlice from "./bill/billSlice";
+// import notificationSlice from "./notification/notificationSlice";
+import { useDispatch } from "react-redux";
+
+export const store = configureStore({
+    reducer: {
+        // user: userSlice.reducer,
+        // resident: residentSlice.reducer,
+        // contract: contractSlice.reducer,
+        // bill: billSlice.reducer,
+        // service: serviceSlice.reducer,
+        // flat: flatSlice.reducer,
+        // building: buildingSlice.reducer,
+        // admin: adminSlice.reducer,
+        // dashboard: dashboardSlice.reducer,
+        // notification: notificationSlice.reducer
+    }
+})
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch: () => AppDispatch = useDispatch // Export a hook that can be reused to resolve types
