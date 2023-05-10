a0bd62cff624111b204396de689317e210c52c1d
Trung Hieu <nthieu1332002@gmail.com>
2023-05-10 12:36:26 +0700

    .

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 // import contractSlice from "./contract/contractSlice";
 // import residentSlice from "./resident/residentSlice";
 // import serviceSlice from "./service/serviceSlice";
